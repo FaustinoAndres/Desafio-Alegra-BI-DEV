db19a76364593a84b7162b6aa7e97cb66ed6106a
Faustino Correa <faustino.correa.m@gmail.com>
2022-06-24 11:43:58 -0400

    obsequio c

--- sql/question_04.sql
+++ sql/question_04.sql
@@ -33,3 +33,23 @@ LIMIT 1;
 SELECT CLIENTE.ID, CLIENTE.nombre, CLIENTE.edad, CLIENTE.genero
 FROM CLIENTE
 WHERE CLIENTE.ID = 17;
+
+-- Obséquio C: Clientes estrellas.
+-- Ranking con los 3 clientes que dejaron más ganancias.
+
+SELECT 		clienteid AS "id",
+			ROUND(SUM(cantidad*precio_unitario*(1.-DESCUENTO.descuento))) AS "Total_compras",
+			ROUND(SUM(cantidad*precio_unitario*(1.-DESCUENTO.descuento))*0.04) AS "obsequio_C"
+FROM VENTAS
+LEFT JOIN DESCUENTO
+ON VENTAS.descuentoid = DESCUENTO.id
+LEFT JOIN CLIENTE
+ON CLIENTE.id = VENTAS.clienteid
+WHERE EXTRACT(YEAR FROM VENTAS.fecha_de_venta) = 2012
+GROUP BY clienteid
+ORDER BY "Total_compras" DESC
+LIMIT 3;
+
+SELECT CLIENTE.ID, CLIENTE.nombre, CLIENTE.edad, CLIENTE.genero
+FROM CLIENTE
+WHERE CLIENTE.ID IN (13, 12, 27);
